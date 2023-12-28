903dcd946fde5554e72bdaf1afa3b9a93bad246c
Smit1980 <117649003+Smit1980@users.noreply.github.com>
2023-12-28 06:29:34 +0300

    script.js

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 const container = document.getElementById("text-container");
 
 const lines = [
-    { text: "<strong><u>Недвижимость:</u></strong>", tag: true },
-    "   -  Экспертиза и управление недвижимыми активами",
-    "   -  Участие в электронных торгах (ФЗ №127, ФЗ №159,ФЗ №229)",
-    "   -  Readivelopment",
-    "   -  Портфельные инвестиции",
-    "   -  Франчайзинг",
-    "   -  Юридическая экспертиза в области недвижимости",
-    "   -  Финансовый анализ и моделирование",
-    "   -  Стратегическое планирование и консалтинг",
-    "   -  Налоговый консалтинг, выявление и возврат налоговых переплат",
+    { text: "Недвижимость:", tag: "<strong><u>" },
+     "    Экспертиза и управление недвижимыми активами",
+     "    Выкуп имущества с электронных торгов (ФЗ №127, ФЗ №159,ФЗ №229)",
+     "    Readivelopment",
+     "    Портфельные инвестиции",
+    "    Юридическая экспертиза в области недвижимости",
+    "    Стратегическое планирование и консалтинг",
+    "    Налоговый консалтинг, выявление и возврат налоговых переплат",
     "",
-    { text: "<strong><u>Information Technology:</u></strong>", tag: true },
-    "   -  Разработка продуктов любой сложности",
-    "   -  Создание инновационных решений",
-    "   -  Уникальные AI-решения и профессиональные инструменты для автоматизации и оптимизации бизнес-процессов",
-    "   -  Глубокое машинное обучение и компьютерное зрение, ИИ",
-    "   -  Лицензирование профессионального программного обеспечения",
-    "   -  Оформления подписок на иностранные программные продукты",
-    "   -  Разработка систем контроля", 
-    "   -  Blockchain and Cryptocurrency - Market Asset Transactions"
+    { text: "Information Technology:", tag: "<strong><u>" },
+    "    Разработка продуктов любой сложности",
+    "    Создание инновационных решений",
+    "    Уникальные AI-решения и профессиональные инструменты для автоматизации и оптимизации бизнес-процессов",
+    "    Глубокое машинное обучение и компьютерное зрение, ИИ",
+    "    Лицензирование профессионального программного обеспечения",
+    "    Оформления подписок на иностранные программные продукты",
+    "    Разработка систем контроля", 
+    "    Blockchain and Cryptocurrency - Market Asset Transactions", 
 ];
 
 let currentLine = 0;
 let index = 0;
+let isTag = false;
 
 function typeWriter() {
     if (currentLine < lines.length) {
         let line = lines[currentLine];
-        if (line.tag) {
-            container.innerHTML += line.text;
-            container.innerHTML += '<br>';
-            currentLine++;
-            setTimeout(typeWriter, 250);
-            return;
+        if (typeof line === 'object') {
+            container.innerHTML += line.tag;
+            line = line.text;
         }
 
         if (index < line.length) {
@@ -42,15 +38,16 @@ function typeWriter() {
             index++;
             setTimeout(typeWriter, 25);
         } else {
+            if (typeof lines[currentLine] === 'object') {
+                container.innerHTML += "</u></strong>";
+            }
             container.innerHTML += '<br>';
             index = 0;
             currentLine++;
             setTimeout(typeWriter, 250);
-
-            // Автоматическая прокрутка вниз
-            container.scrollTop = container.scrollHeight;
         }
     }
 }
 
 typeWriter();
+
